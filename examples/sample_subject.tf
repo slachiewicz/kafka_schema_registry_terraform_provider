2f2113dfdf193105a28035aa980f69394ac7ad0c
luiz.portela <luiz.portela@rubylife.com>
2021-11-20 20:24:59 -0500

    fix

--- examples/sample_subject.tf
+++ examples/sample_subject.tf
@@ -11,28 +11,23 @@ provider "schemaregistry" {
   uri = "http://localhost:8081"
 }
 
-//resource "schemaregistry_subject" "schema_sample_from_string" {
-//  subject = "com.test.myapp.test-from-string"
-//  schema  = "{\"type\":\"record\",\"name\":\"paymentsxuxa\",\"namespace\":\"my.examples\",\"fields\":[{\"name\":\"id\",\"type\":\"string\"},{\"name\":\"amount\",\"type\":\"double\"}]}"
-//}
-//
+resource "schemaregistry_subject" "schema_sample_from_string" {
+  subject = "com.test.myapp.test-from-string"
+  schema  = "{\"type\":\"record\",\"name\":\"payments\",\"namespace\":\"my.examples\",\"fields\":[{\"name\":\"id\",\"type\":\"string\"},{\"name\":\"amount\",\"type\":\"double\"}]}"
+}
+
 resource "schemaregistry_config" "schema_sample_from_string_config" {
   subject = "com.test.myapp.test-from-string"
   config  = "{\"compatibility\":\"FORWARD\"}"
 }
 
-//
+// from file need review
 //resource "schemaregistry_subject" "schema_sample_from_file" {
 //  subject = "com.test.myapp.test-from-file"
 //  schema  = "${file("schema_sample.avro.json")}"
 //}
 
-//resource "schemaregistry_config" "schema_sample_config" {
-//  subject = "com.test.myapp.test-from-string"
-//  config  = "{\"compatibility\":\"FORWARD\"}"
-//}
-
-#resource "schemaregistry_config" "schema_sample_config_2" {
-#  subject = "com.test.myapp.test-from-string-2"
-#  config  = "{\"compatibility\":\"BACKWARD\"}"
-#}
+resource "schemaregistry_config" "schema_sample_config_2" {
+  subject = "com.test.myapp.test-from-string-2"
+  config  = "{\"compatibility\":\"BACKWARD\"}"
+}
